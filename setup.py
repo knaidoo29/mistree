0a8ff3f0ec720b83fa23a1afc3a54c83502a8108
Krishna Naidoo <krishna.naidoo.11@ucl.ac.uk>
2024-11-28 01:57:11 +0000

    edit to setup.py for pypi upload

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
 setup(
     name="mistree",
     version="2.0.0",
     description= "A python package for constructing and analysing the minimum spanning tree",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://knaidoo29.github.io/mistreedoc/',
     author="Krishna Naidoo",
     author_email="krishna.naidoo.11@ucl.ac.uk",
