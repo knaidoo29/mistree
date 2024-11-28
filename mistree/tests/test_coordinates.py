fix windows test numerical precision error, now round all to 6 decimal places

mistree/tests/test_coordinates.py
@@ -54,7 +54,7 @@ def test_celestial_2_cartesian():
     assert round(z, 6) == 1.
     phi, theta, x, y, z = mist.celestial_2_cartesian(1., 0., 0.)
     assert phi == 0.
-    assert theta == round(np.pi/2., 6)
+    assert round(theta, 6) == round(np.pi/2., 6)
     assert round(x, 6) == 1.
     assert round(y, 6) == 0.
     assert round(z, 6) == 0.
@@ -64,7 +64,7 @@ def test_celestial_2_cartesian():
     assert round(z, 6) == 0.
     phi, theta, x, y, z = mist.celestial_2_cartesian(1., 0., 45.)
     assert phi == 0.
-    assert theta == round(np.pi/4., 6)
+    assert round(theta, 6) == round(np.pi/4., 6)
     assert round(x, 6) == round(np.sqrt(2.)/2., 6)
     assert round(y, 6) == 0.
     assert round(z, 6) == round(np.sqrt(2.)/2., 6)
@@ -74,7 +74,7 @@ def test_celestial_2_cartesian():
     assert round(z, 6) == round(np.sqrt(2.)/2., 6)
     phi, theta, x, y, z = mist.celestial_2_cartesian(1., 0., np.pi/2., units='radians')
     assert phi == 0.
-    assert theta == 0.
+    assert round(theta, 6) == 0.
     assert round(x, 6) == 0.
     assert round(y, 6) == 0.
     assert round(z, 6) == 1.
@@ -84,7 +84,7 @@ def test_celestial_2_cartesian():
     assert round(z, 6) == 1.
     phi, theta, x, y, z = mist.celestial_2_cartesian(1., 0., 0., units='radians')
     assert phi == 0.
-    assert theta == round(np.pi/2., 6)
+    assert round(theta, 6) == round(np.pi/2., 6)
     assert round(x, 6) == 1.
     assert round(y, 6) == 0.
     assert round(z, 6) == 0.
@@ -94,7 +94,7 @@ def test_celestial_2_cartesian():
     assert round(z, 6) == 0.
     phi, theta, x, y, z = mist.celestial_2_cartesian(1., 0., np.pi/4., units='radians')
     assert phi == 0.
-    assert theta == round(np.pi/4., 6)
+    assert round(theta, 6) == round(np.pi/4., 6)
     assert round(x, 6) == round(np.sqrt(2.)/2., 6)
     assert round(y, 6) == 0.
     assert round(z, 6) == round(np.sqrt(2.)/2., 6)
@@ -167,7 +167,7 @@ def test_celestial_2_unit_sphere():
     assert round(z, 6) == 1.
     phi, theta, x, y, z = mist.celestial_2_unit_sphere(0., 0.)
     assert phi == 0.
-    assert theta == round(np.pi/2., 6)
+    assert round(theta, 6) == round(np.pi/2., 6)
     assert round(x, 6) == 1.
     assert round(y, 6) == 0.
     assert round(z, 6) == 0.
@@ -177,7 +177,7 @@ def test_celestial_2_unit_sphere():
     assert round(z, 6) == 0.
     phi, theta, x, y, z = mist.celestial_2_unit_sphere(0., 45.)
     assert phi == 0.
-    assert theta == round(np.pi/4., 6)
+    assert round(theta, 6) == round(np.pi/4., 6)
     assert round(x, 6) == round(np.sqrt(2.)/2., 6)
     assert round(y, 6) == 0.
     assert round(z, 6) == round(np.sqrt(2.)/2., 6)
@@ -197,7 +197,7 @@ def test_celestial_2_unit_sphere():
     assert round(z, 6) == 1.
     phi, theta, x, y, z = mist.celestial_2_unit_sphere(0., 0., units='radians')
     assert phi == 0.
-    assert theta == round(np.pi/2., 6)
+    assert round(theta, 6) == round(np.pi/2., 6)
     assert round(x, 6) == 1.
     assert round(y, 6) == 0.
     assert round(z, 6) == 0.
@@ -207,7 +207,7 @@ def test_celestial_2_unit_sphere():
     assert round(z, 6) == 0.
     phi, theta, x, y, z = mist.celestial_2_unit_sphere(0., np.pi/4., units='radians')
     assert phi == 0.
-    assert theta == round(np.pi/4., 6)
+    assert round(theta, 6) == round(np.pi/4., 6)
     assert round(x, 6) == round(np.sqrt(2.)/2., 6)
     assert round(y, 6) == 0.
     assert round(z, 6) == round(np.sqrt(2.)/2., 6)